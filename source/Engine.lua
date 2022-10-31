8cf564f29be9b6b9cb5755e255af16f2030fe707
Didier Malenfant <coding@malenfant.net>
2022-10-31 09:42:00 +0100

    Whitespace

--- source/Engine.lua
+++ source/Engine.lua
@@ -1,4 +1,4 @@
--- SPDX-FileCopyrightText: 2022-present  Didier Malenfant <coding@malenfant.net>
+-- SPDX-FileCopyrightText: 2022-present Didier Malenfant <coding@malenfant.net>
 --
 -- SPDX-License-Identifier: MIT
 
